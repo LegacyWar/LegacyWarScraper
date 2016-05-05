c23e00696faffee3551b4dd830ba8027dccdcc1a
Vasja Volin <vasja.volin@gmail.com>
2016-05-05 16:23:31 -0400

    add pulling config from json file

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from github import Github
+import json
 
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
+username = config['github']['username']
+password = config['github']['password']
+
+g = Github(username, password);
+
+for repo in g.get_user().get_repos():
+    print repo.name
