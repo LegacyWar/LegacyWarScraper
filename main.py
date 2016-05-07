2aa9b91db5542f2dd501d6f9dc1180745ce47470
Vasja Volin <vasja.volin@gmail.com>
2016-05-06 23:43:09 -0400

    add a sample endpoint for viewing what we are working with

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from github import Github
 import json
+import pymongo
+from flask import Flask
+
+app = Flask(__name__)
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -7,7 +11,16 @@ with open('config.json') as config_file:
 username = config['github']['username']
 password = config['github']['password']
 
-g = Github(username, password);
 
-for repo in g.get_user().get_repos():
-    print repo.name
+@app.route('/')
+def data():
+    g = Github(username, password);
+
+    items = []
+    for repo in g.get_user().get_repos():
+        items.append(repo.name)
+
+    return json.dumps(items)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
